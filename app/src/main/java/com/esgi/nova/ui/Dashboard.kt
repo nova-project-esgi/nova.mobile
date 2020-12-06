522d77c06ba65cf16525eb45dee9b6a398d2bce0
Maxime Antoine <mantoine1@myges.fr>
2020-12-06 15:59:07 +0100

    basic dashboard

---
+++ app/src/main/java/com/esgi/nova/ui/Dashboard.kt
@@ -0,0 +1,12 @@
+package com.esgi.nova.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.esgi.nova.R
+
+class Dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
