fd603ff64d22dc00b2b171f53037e4727ad2d445
JBertho <james.bertho94@gmail.com>
2020-12-06 20:06:41 +0100

    dashboard front v1

--- app/src/main/java/com/esgi/nova/Dashboard.kt
+++ app/src/main/java/com/esgi/nova/Dashboard.kt
@@ -2,11 +2,16 @@ package com.esgi.nova
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.esgi.nova.R
+import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        spn_difficulty.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, listOf("Facile","Moyen","Difficile"))
+        spn_resources.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, listOf("30","50","70"))
     }
 }
