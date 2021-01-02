d43267eb0a55764e08c7a3372093ddf674d31467
JBertho <james.bertho94@gmail.com>
2021-01-02 17:56:46 +0100

    login page with material input

--- app/src/main/java/com/esgi/nova/Login.kt
+++ app/src/main/java/com/esgi/nova/Login.kt
@@ -3,7 +3,9 @@ package com.esgi.nova
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity(), View.OnClickListener {
@@ -16,6 +18,9 @@ class Login : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(view: View?) {
         if (view == btn_login) {
+            Log.d("TEST", ti_login.text.toString())
+            Log.d("TEST", ti_password.text.toString())
+
             val intent = Intent(this, Dashboard::class.java)
             startActivity(intent);
         }
