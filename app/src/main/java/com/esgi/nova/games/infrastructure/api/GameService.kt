7fead445cbb0da7723b1f53dd0c4d97f78835b0c
BAILLEUL <bailleul.sacha@gmail.com>
2021-01-16 20:55:28 +0100

    [feat]: Get leaderboard by difficulty

--- app/src/main/java/com/esgi/nova/games/infrastructure/api/GameService.kt
+++ app/src/main/java/com/esgi/nova/games/infrastructure/api/GameService.kt
@@ -2,8 +2,11 @@ package com.esgi.nova.games.infrastructure.api
 
 import com.esgi.nova.games.application.models.GameForCreation
 import com.esgi.nova.games.infrastructure.api.models.GameForUpdate
+import com.esgi.nova.games.infrastructure.dto.LeaderBoardGameView
 import com.esgi.nova.games.ports.IGameForCreation
 import com.esgi.nova.infrastructure.api.ApiConstants
+import com.esgi.nova.infrastructure.api.HeaderConstants
+import com.esgi.nova.infrastructure.api.pagination.PageMetadata
 import com.esgi.nova.models.Score
 import okhttp3.ResponseBody
 import retrofit2.Call
@@ -13,6 +16,10 @@ import java.util.*
 interface GameService {
     @GET("json/get/Ey-Bp0E3F?delay=2000")
     fun retrieveUser(): Call<Score>?
+
+    @Headers(HeaderConstants.Accept + ": " + ApiConstants.CustomMediaType.Application.LeaderBoardGame)
+    @GET("${ApiConstants.BaseUrl}${ApiConstants.EndPoints.Games}")
+    fun getLeaderBoardGamesByDifficulty(@Query("difficultyId") difficultyId: String): Call<PageMetadata<LeaderBoardGameView>>
 
     @POST(ApiConstants.EndPoints.Games)
     fun createGame(@Body gameForCreation: GameForCreation): Call<ResponseBody>
